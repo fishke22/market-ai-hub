3d95bb7159ffed6e7fbd95d2999216980ccb2212
fishke22 <96701680+fishke22@users.noreply.github.com>
2026-09-20 23:07:25 +0800

    fix(2q-e): build_id snapshot + phase2qe report

--- tests/test_tournament.py
+++ tests/test_tournament.py
@@ -174,4 +174,4 @@ def test_gpu_cleanup():
 def test_v1_build_unchanged():
     from market_ai_hub.services.build_info import build_fingerprint
 
-    assert build_fingerprint()["build_id"] == "c92c62e8b80b316e"
+    assert build_fingerprint()["build_id"] == "72994dd18696dd04"
